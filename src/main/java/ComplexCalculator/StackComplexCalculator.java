222ded4a96c5f63711ae5b546a6b73ff01f790aa
P11etro <pietro.pantani.03@gmail.com>
2024-02-19 01:00:52 +0100

    enabled custom font

--- src/main/java/ComplexCalculator/StackComplexCalculator.java
+++ src/main/java/ComplexCalculator/StackComplexCalculator.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 /** The StackComplexCalculator is the main class.
@@ -16,6 +17,7 @@ public class StackComplexCalculator extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+        Font.loadFont(getClass().getResource("Inter-Regular.ttf").toExternalForm(), 10);
         Parent root = FXMLLoader.load(getClass().getResource("ComplexCalculatorView.fxml"));
 
         Scene scene = new Scene(root);
